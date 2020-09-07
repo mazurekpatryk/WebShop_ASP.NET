bdc7105fc61ab2da42220269c323b8be59d1ca6e
Keruzaimx <patrykmazurek13@gmail.com>
2020-09-07 12:31:09 +0200

    Create project

---
+++ WebShop.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebShop.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
