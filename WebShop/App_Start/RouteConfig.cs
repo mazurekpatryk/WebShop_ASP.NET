4aa627353ccb34f11791d0ec6566a34d1a243128
Keruzaimx <patrykmazurek13@gmail.com>
2020-09-07 15:44:29 +0200

    Add first pages and views

--- WebShop/App_Start/RouteConfig.cs
+++ WebShop/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace WebShop
@@ -12,6 +8,24 @@ namespace WebShop
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "ProductDetails",
+                url: "produkt-{id}.html",
+                defaults: new { controller = "Store", action = "Details" }
+            );
+
+            routes.MapRoute(
+                name: "StaticPages",
+                url: "strony/{viewname}.html",
+                defaults: new { controller = "Home", action = "StaticContent" }
+            );
+
+            routes.MapRoute(
+                name: "ProductList",
+                url: "lista-produktow/{type}.html",
+                defaults: new { controller = "Store", action = "List" }
+            );
 
             routes.MapRoute(
                 name: "Default",
---
+++ WebShop/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebShop.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult List(string type)
+        {
+            return View();
+        }
+    }
+}
