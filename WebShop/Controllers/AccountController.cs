2885e330ec49ba48ceff094708a8276d2e883f8d
Keruzaimx <patrykmazurek13@gmail.com>
2020-09-14 11:26:14 +0200

    Added login and register

---
+++ WebShop/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShop.ViewModels;
+
+namespace WebShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl= returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModels model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ WebShop/ViewModels/AccountViewModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebShop.ViewModels
+{
+    public class LoginViewModels
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Musisz wprowadzić hasło")]
+        public string Password { get; set; }
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "the {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The Password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
