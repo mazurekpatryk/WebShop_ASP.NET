931b4d1f9a2de24344c346513a93272a053cef81
Keruzaimx <patrykmazurek13@gmail.com>
2020-09-15 12:44:07 +0200

    Added buy history

--- WebShop/DAL/StoreInitializer.cs
+++ WebShop/DAL/StoreInitializer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
@@ -13,7 +15,7 @@ namespace WebShop.DAL
         protected override void Seed(StoreContext context)
         {
             SeedStoreData(context);
-
+            InitializeIdentityForEF(context);
             base.Seed(context);
         }
 
@@ -54,5 +56,50 @@ namespace WebShop.DAL
             context.SaveChanges();
 
         }
+
+        public static void InitializeIdentityForEF(StoreContext db)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            //var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            //var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+            const string name = "admin@admin.pl";
+            const string password = "1qaz@WSX";
+            const string roleName = "Admin";
+
+
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = name, Email = name, UserData = new UserData() };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+
+            //Create Role Admin if it does not exist
+            var role = roleManager.FindByName(roleName);
+            if (role == null)
+            {
+                role = new IdentityRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+
+            //var user = userManager.FindByName(name);
+            //if (user == null)
+            //{
+            //    user = new ApplicationUser { UserName = name, Email = name };
+            //    var result = userManager.Create(user, password);
+            //    result = userManager.SetLockoutEnabled(user.Id, false);
+            //}
+
+            // Add user admin to Role Admin if not already added
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name))
+            {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+
     }
 }
